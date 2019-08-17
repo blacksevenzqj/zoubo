18314d894f2db2ce4b2124457064e1e8ef8c0f1f
blacksevenzqj <397353721@qq.com>
2019-08-18 01:49:48 +0800

    KS阈值

--- 2_seven/MyLearn/1_Cross-validation/MyLearn_Cross-validation.py
+++ 2_seven/MyLearn/1_Cross-validation/MyLearn_Cross-validation.py
@@ -7,7 +7,9 @@ y = digits.target
 
 
 from sklearn.model_selection import train_test_split
+
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=42)
+
 
 from sklearn.neighbors import KNeighborsClassifier
 
