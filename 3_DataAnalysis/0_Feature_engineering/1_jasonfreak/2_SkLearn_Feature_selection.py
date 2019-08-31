7c4d07dd2f7cb8e8e31d8f899e71010afb083f5d
blacksevenzqj <397353721@qq.com>
2019-09-01 00:23:26 +0800

    KS阈值

--- 3_DataAnalysis/0_Feature_engineering/1_jasonfreak/2_SkLearn_Feature_selection.py
+++ 3_DataAnalysis/0_Feature_engineering/1_jasonfreak/2_SkLearn_Feature_selection.py
@@ -25,7 +25,7 @@ vdata = VarianceThreshold(threshold=3).fit_transform(iris.data)
 #       np.square(np.std(iris.data[:,2])), np.square(np.std(iris.data[:,3])))
 
 
-# ============================================================================================================
+# -------------------------------------------------------------------------------------------------------------
 
 
 '''
@@ -46,10 +46,11 @@ sdata = SelectKBest(lambda X, Y: list(np.array([pearsonr(x, Y) for x in X.T]).T)
 # print(sdata[0:5])
 
 '''
-关于 皮尔森相似度 计算的说明：
+关于 皮尔森相似度 计算的说明：（连续-连续）
 1、自变量（连续）之间的 pearson相似度分析；
-2、自变量（连续） 与 因变量（连续）之间的 pearson相关度分析：鸢尾花数据的 因变量Y 是 分类变量，只不过是用数值表示的（普通转换）。
-这种情况下把 因变量Y 当做是 连续变量 进行皮尔森相似度计算（是否需要将 因变量Y 做WOE转换？）。
+2、自变量（连续） 与 因变量（连续）之间的 pearson相关度分析：鸢尾花数据的 因变量Y 是 分类变量，是用数值表示的（普通转换）。
+这种情况下把 因变量Y 当做是 连续变量 进行皮尔森相似度计算（是否需要将 因变量Y 做WOE转换？
+之前做的是 自变量X（分类）WOE转换 为 连续变量 后 做PCA）。
 '''
 # 3.1.2.1、pearsonr皮尔森相关系数矩阵的测试：
 # 3.1.2.1.1、pd.DataFrame格式：
@@ -72,34 +73,156 @@ idatap[4] = iris.target
 2、np.corrcoef(矩阵A)中 矩阵A 必须是：行是特征，列是样本
 '''
 # 3.1.2.1.2.1、以 np.vstack：按垂直方向（行顺序）堆叠数组构成一个新的数组
-ixh = iris.data[:, 0] # 特征 行格式
-iyh = iris.target # 因变量 行格式
-idatal = np.vstack((ixh, iyh)) # 垂直方向（行顺序）堆叠，正好符合corrcoef函数要求
-print(idatal[0:5])
-print(np.corrcoef(idatal)) # 行是特征，列是样本
+# ixh = iris.data[:, 0] # 特征 行格式
+# iyh = iris.target # 因变量 行格式
+# idatal = np.vstack((ixh, iyh)) # 垂直方向（行顺序）堆叠，正好符合corrcoef函数要求
+# print(idatal[0:5])
+# print(np.corrcoef(idatal)) # 行是特征，列是样本
 
 # 3.1.2.1.2.2、以 np.hstack：按水平方向（列顺序）堆叠数组构成一个新的数组
 # 如果使用 水平方向（列顺序）堆叠：必须两个矩阵都是二维，才能堆叠出正确格式
 # 3.1.2.1.2.2.1、将 因变量Y 按水平方向 堆叠进矩阵，方便其余特征分别与因变量Y进行计算
-iY2w = iris.target[np.newaxis, :] # 一维扩展为二维，行格式
-idatah = np.hstack((iris.data, iY2w.T)) # 注意，Y做转置T，转置为列
-print(idatah[0:5])
-idatahf = idatah[:,[0,4]] # 选择 矩阵0列（特征X1）和 矩阵4列（因变量Y）
-print(idatahf[0:5])
-print(np.corrcoef(idatahf.T)) # .T 转置为 行是特征，列是样本
+# iY2w = iris.target[np.newaxis, :] # 一维扩展为二维，行格式
+# idatah = np.hstack((iris.data, iY2w.T)) # 注意，Y做转置T，转置为列
+# print(idatah[0:5])
+# idatahf = idatah[:,[0,4]] # 选择 矩阵0列（特征X1）和 矩阵4列（因变量Y）
+# print(idatahf[0:5])
+# print(np.corrcoef(idatahf.T)) # .T 转置为 行是特征，列是样本
 
 # 3.1.2.1.2.2.2、单独拿出一个特征进行计算：
-iX02w = iris.data[:, 0][np.newaxis, :] # 一维扩展为二维，行格式
-iY2w = iris.target[np.newaxis, :] # 一维扩展为二维，行格式
-idatah = np.hstack((iX02w.T, iY2w.T)) # .T 转置为 列格式 合并
-print(idatah[0:5])
-print(np.corrcoef(idatah.T)) # .T 转置为 行是特征，列是样本
+# iX02w = iris.data[:, 0][np.newaxis, :] # 一维扩展为二维，行格式
+# iY2w = iris.target[np.newaxis, :] # 一维扩展为二维，行格式
+# idatah = np.hstack((iX02w.T, iY2w.T)) # .T 转置为 列格式 合并
+# print(idatah[0:5])
+# print(np.corrcoef(idatah.T)) # .T 转置为 行是特征，列是样本
 
 # -------------------------------------------------------------------------------------------------------------
 
-# 3.1.3、方检验
+# 3.1.3、方检验（分类-分类）
 # 经典的卡方检验是检验 定性自变量（分类） 对 定性因变量（分类） 的相关性。假设自变量有N种取值，因变量有M种取值，
 # 考虑自变量等于i且因变量等于j的样本频数的观察值与期望的差距，构建统计量：
+# from sklearn.feature_selection import SelectKBest
+# from sklearn.feature_selection import chi2
+# # 选择K个最好的特征，返回选择特征后的数据
+# kfdata = SelectKBest(chi2, k=3).fit_transform(iris.data, iris.target)
+# print(kfdata[0:5]) # 卡方检验计算结果 和 皮尔森相似度计算结果相同
+
+# -------------------------------------------------------------------------------------------------------------
+
+# 3.1.4、互信息法（分类-分类）包装不起来，暂时不管了
+# from sklearn.feature_selection import SelectKBest
+# from minepy import MINE
+# #由于MINE的设计不是函数式的，定义mic方法将其为函数式的，返回一个二元组，二元组的第2项设置成固定的P值0.5
+# def mic(x, y):
+#     m = MINE()
+#     m.compute_score(x, y)
+#     return (m.mic(), 0.5)
+# #选择K个最好的特征，返回特征选择后的数据
+# SelectKBest(lambda X, Y: array(map(lambda x:mic(x, Y), X.T)).T, k=2).fit_transform(iris.data, iris.target)
 
 
+# ********************************************************************************************************
 
+
+'''
+B、循环特征选取：
+不单独地检验某个特征的价值，而是检验特征集的价值。对于一个数量为n的特征集合，子集的个数为2的n次方减一。
+通过指定一个学习算法，通过算法计算所有子集的error，选择error最小的子集作为选取的特征。
+1、对初始特征集合中每个特征赋予一个初始权重。
+2、训练，将权重最小的特征移除。
+3、不断迭代，直到特征集合的数目达到预定值。
+'''
+
+# 3.2、Wrapper
+
+# 3.2.1、递归特征消除法
+# 递归消除特征法使用一个基模型来进行多轮训练，每轮训练后，消除若干权值系数的特征，再基于新的特征集进行下一轮训练。
+# 使用feature_selection库的RFE类来选择特征的代码如下：
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+# 递归特征消除法，返回特征选择后的数据
+# 参数estimator为基模型
+# 参数n_features_to_select为选择的特征个数
+refdata = RFE(estimator=LogisticRegression(), n_features_to_select=3).fit_transform(iris.data, iris.target)
+# print(refdata[0:5]) # 这里选择了 1、2、3特征，和上面的方法结果有区别。
+
+
+# ============================================================================================================
+
+
+# 3.3、Embedded
+
+# 3.3.1 基于惩罚项的特征选择法
+# 使用带惩罚项的基模型，除了筛选出特征外，同时也进行了降维。
+# 使用feature_selection库的SelectFromModel类结合带L1惩罚项的逻辑回归模型，来选择特征的代码如下：
+from sklearn.feature_selection import SelectFromModel
+from sklearn.linear_model import LogisticRegression
+#带L1惩罚项的逻辑回归作为基模型的特征选择
+sl1data = SelectFromModel(LogisticRegression(penalty="l1", C=0.1)).fit_transform(iris.data, iris.target)
+# print(sl1data[0:5])
+
+# L1惩罚项降维的原理在于保留多个对目标值具有同等相关性的特征中的一个，所以没选到的特征不代表不重要。
+# 故，可结合L2惩罚项来优化。具体操作为：若一个特征在L1中的权值为1，选择在L2中权值差别不大且在L1中权值为0的特征构成同类集合，
+# 将这一集合中的特征平分L1中的权值，故需要构建一个新的逻辑回归模型：
+from sklearn.linear_model import LogisticRegression
+
+class LR(LogisticRegression):
+    def __init__(self, threshold=0.01, dual=False, tol=1e-4, C=1.0,
+                 fit_intercept=True, intercept_scaling=1, class_weight=None,
+                 random_state=None, solver='liblinear', max_iter=100,
+                 multi_class='ovr', verbose=0, warm_start=False, n_jobs=1):
+
+        #权值相近的阈值
+        self.threshold = threshold
+        LogisticRegression.__init__(self, penalty='l1', dual=dual, tol=tol, C=C,
+                 fit_intercept=fit_intercept, intercept_scaling=intercept_scaling, class_weight=class_weight,
+                 random_state=random_state, solver=solver, max_iter=max_iter,
+                 multi_class=multi_class, verbose=verbose, warm_start=warm_start, n_jobs=n_jobs)
+        #使用同样的参数创建L2逻辑回归
+        self.l2 = LogisticRegression(penalty='l2', dual=dual, tol=tol, C=C, fit_intercept=fit_intercept, intercept_scaling=intercept_scaling, class_weight = class_weight, random_state=random_state, solver=solver, max_iter=max_iter, multi_class=multi_class, verbose=verbose, warm_start=warm_start, n_jobs=n_jobs)
+
+    def fit(self, X, y, sample_weight=None):
+        #训练L1逻辑回归
+        super(LR, self).fit(X, y, sample_weight=sample_weight)
+        self.coef_old_ = self.coef_.copy()
+        #训练L2逻辑回归
+        self.l2.fit(X, y, sample_weight=sample_weight)
+
+        cntOfRow, cntOfCol = self.coef_.shape
+        #权值系数矩阵的行数对应目标值的种类数目
+        for i in range(cntOfRow):
+            for j in range(cntOfCol):
+                coef = self.coef_[i][j]
+                #L1逻辑回归的权值系数不为0
+                if coef != 0:
+                    idx = [j]
+                    #对应在L2逻辑回归中的权值系数
+                    coef1 = self.l2.coef_[i][j]
+                    for k in range(cntOfCol):
+                        coef2 = self.l2.coef_[i][k]
+                        #在L2逻辑回归中，权值系数之差小于设定的阈值，且在L1中对应的权值为0
+                        if abs(coef1-coef2) < self.threshold and j != k and self.coef_[i][k] == 0:
+                            idx.append(k)
+                    #计算这一类特征的权值系数均值
+                    mean = coef / len(idx)
+                    self.coef_[i][idx] = mean
+        return self
+
+# 使用feature_selection库的SelectFromModel类结合带L1以及L2惩罚项的逻辑回归模型，来选择特征的代码如下：
+from sklearn.feature_selection import SelectFromModel
+#带L1和L2惩罚项的逻辑回归作为基模型的特征选择
+#参数threshold为权值系数之差的阈值
+sl1l2data = SelectFromModel(LR(threshold=0.5, C=0.1)).fit_transform(iris.data, iris.target)
+# print(sl1l2data)
+
+
+# ----------------------------------------------------------------------------------------------------------------------------
+
+
+# 3.3.2、基于树模型的特征选择法
+# 树模型中GBDT也可用来作为基模型进行特征选择，使用feature_selection库的SelectFromModel类结合GBDT模型，来选择特征的代码如下：
+from sklearn.feature_selection import SelectFromModel
+from sklearn.ensemble import GradientBoostingClassifier
+# GBDT作为基模型的特征选择
+sgbdtdata = SelectFromModel(GradientBoostingClassifier()).fit_transform(iris.data, iris.target)
+# print(sgbdtdata[0:5])
