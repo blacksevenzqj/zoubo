bc500a269a4b008d658a24ccf61526ba8ec25b58
blacksevenzqj <397353721@qq.com>
2019-09-17 00:49:39 +0800

    KS阈值

--- 3_DataAnalysis/0_Feature_engineering/101_My_Learn/2_My_Sampling/3_Cross-validation/MyLearn_Cross-validation.py
+++ 3_DataAnalysis/0_Feature_engineering/101_My_Learn/2_My_Sampling/3_Cross-validation/MyLearn_Cross-validation.py
@@ -9,7 +9,8 @@ y = digits.target
 from sklearn.model_selection import train_test_split
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=42)
-
+print(x_train.shape, y_train.shape)
+print(x_test.shape, y_test.shape)
 
 from sklearn.neighbors import KNeighborsClassifier
 
@@ -20,8 +21,8 @@ from sklearn.neighbors import KNeighborsClassifier
 best_score, best_p, best_k = 0, 0 ,0
 for k in range(2, 11):
     for p in range(1, 6):
-        knn_clf = KNeighborsClassifier(weights="distance", n_neighbors=k, p=p)
-        knn_clf.fit(x_train, y_train)
+        knn_clf = KNeighborsClassifier(weights="distance", n_neighbors=k, p=p) # 定义模型时，手动设定的是 超参数
+        knn_clf.fit(x_train, y_train) # 模型实例.fit()训练出一个模型（其实就是得到 模型的参数。注意是参数，不是超参数）
         score = knn_clf.score(x_test, y_test)
         if score > best_score:
             best_score, best_p, best_k = score, p, k
@@ -50,7 +51,7 @@ knn_clf = KNeighborsClassifier()
 best_score, best_p, best_k = 0, 0 ,0
 for k in range(2, 11):
     for p in range(1, 6):
-        knn_clf = KNeighborsClassifier(weights="distance", n_neighbors=k, p=p)
+        knn_clf = KNeighborsClassifier(weights="distance", n_neighbors=k, p=p)  # 定义模型时，手动设定的是 超参数
         '''
         一、cross_val_score函数参数：
         1、scoring参数：
@@ -66,10 +67,14 @@ for k in range(2, 11):
         二、KFold函数参数：
         1、设置shuffle=True时，多次运行的结果不同。
         2、设置shuffle=True和random_state=整数，发现每次运行的结果都相同
+        
+        三、理解：
+        单纯从 cross_val_score 来看，每一折交叉验证 模型实例.fit()训练出一个模型（其实就是得到 模型的参数。注意是参数，不是超参数） 
+        超参数 是在定义模型时就已经设置好的，不由 cross_val_score 交叉验证 控制。
         '''
         kf = KFold(3, shuffle=True).get_n_splits(x_train)
         # accuracy 准确率指标。当样本比例不平衡时，该指标不准确。
-        score = cross_val_score(knn_clf, x_train, y_train, scoring='accuracy', cv=kf).mean() # 3折交叉验证，有3个准确度分数
+        score = cross_val_score(knn_clf, x_train, y_train, scoring='accuracy', cv=kf).mean() # 3折交叉验证，有3个准确度分数；注意：这里求了mean()
         if score > best_score:
             best_score, best_p, best_k = score, p, k
 
@@ -85,8 +90,8 @@ print("cross_val_score测试结果：", testScore)
 
 
 
-# 3、使用交叉验证（使用 网格搜索）：
-'''
+# 3、使用交叉验证（使用 网格搜索）：同时得到：1、最优模型参数、2、最优模型的超参数
+# '''
 from sklearn.model_selection import GridSearchCV
 
 # estimator：创建的对象，如上的knn_clf
@@ -100,6 +105,7 @@ from sklearn.model_selection import GridSearchCV
 # 可以生成训练集与测试集的迭代器：同上，略
 
 knn_clf = KNeighborsClassifier()
+# param_grid 为模型的超参数，不是参数。
 param_grid = [
     {
         'weights':['distance'],
@@ -109,11 +115,18 @@ param_grid = [
 ]
 
 grid_search = GridSearchCV(knn_clf, param_grid, verbose=1, cv=3)
-grid_search.fit(x_train, y_train)
+# 这样测试就为了观测 使用 最优模型 再训练数据 的情况
+grid_search.fit(x_train[0:600], y_train[0:600]) # 网格搜索 同时得到：1、最优模型参数、2、最优模型的超参数
 
 print(grid_search.best_score_)
 print(grid_search.best_params_)
 best_knn_clf = grid_search.best_estimator_ # 最佳分类器
+print(best_knn_clf)
 testScore = best_knn_clf.score(x_test, y_test)
 print("GridSearchCV测试结果：", testScore)
-'''
+
+
+best_knn_clf.fit(x_train[600:1078], y_train[600:1078])
+print(best_knn_clf) # 可以看出 最优模型的超参数保留，改变的只是最优模型的参数
+print("GridSearchCV测试结果：", best_knn_clf.score(x_test, y_test))
+# '''
