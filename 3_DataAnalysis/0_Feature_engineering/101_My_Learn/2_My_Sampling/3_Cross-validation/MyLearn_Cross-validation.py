0ff5c64a7b48ae4884b547fec06867754f98bfb3
blacksevenzqj <397353721@qq.com>
2019-09-24 14:31:56 +0800

    KS阈值

--- 3_DataAnalysis/0_Feature_engineering/101_My_Learn/2_My_Sampling/3_Cross-validation/MyLearn_Cross-validation.py
+++ 3_DataAnalysis/0_Feature_engineering/101_My_Learn/2_My_Sampling/3_Cross-validation/MyLearn_Cross-validation.py
@@ -34,64 +34,64 @@ print("Best Score =", best_score)
 
 
 
-# # 2、使用交叉验证：
-# from sklearn.model_selection import cross_val_score
-# from sklearn.model_selection import KFold
-# '''
-# KFold 测试：
-# 1、设置shuffle=True时，运行两次，发现多次运行的结果不同。
-# 2、设置shuffle=True和random_state=整数，发现每次运行的结果都相同
-# data = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6])
-# # prepare cross validation
-# kfold = KFold(n_splits=3, shuffle = True, random_state=42)
-# for train, test in kfold.split(data):
-#     print('train: %s, test: %s' % (data[train], data[test]))
-# '''
-# knn_clf = KNeighborsClassifier()
-# best_score, best_p, best_k = 0, 0 ,0
-# for k in range(2, 11):
-#     for p in range(1, 6):
-#         knn_clf = KNeighborsClassifier(weights="distance", n_neighbors=k, p=p)  # 定义模型时，手动设定的是 超参数
-#         '''
-#         一、cross_val_score函数参数：（cross_val_score 只能和 KFold 配合使用，不能和 StratifiedKFold 配合使用）
-#         1、scoring参数：
-#         1.1、一般来说平均方差(Mean squared error)会用于判断回归(Regression)模型的好坏
-#         scoring='mean_squared_error'
-#         1.2、平均准确率
-#         scoring='accuracy
-#         2、cv参数：
-#         cv=3：分成3份做交叉验证，2份做训练，1份做验证
-#         交叉验证接口sklearn.model_selection.cross_validate没有数据shuffle功能，所以一般结合Kfold一起使用，
-#         KFold是做分割数据作用，相当于cross_val_score的cv参数；做交叉验证是cross_val_score。
-#
-#         二、KFold函数参数：
-#         1、设置shuffle=True时，多次运行的结果不同。
-#         2、设置shuffle=True和random_state=整数，发现每次运行的结果都相同
-#
-#         三、理解：
-#         单纯从 cross_val_score 来看，每一折交叉验证 模型实例.fit()训练出一个模型（其实就是得到 模型的参数。注意是参数，不是超参数）
-#         超参数 是在定义模型时就已经设置好的，不由 cross_val_score 交叉验证 控制。
-#         '''
-#         kf = KFold(3, shuffle=True)
-#         # accuracy 准确率指标。当样本比例不平衡时，该指标不准确。
-#         score = cross_val_score(knn_clf, x_train, y_train, scoring='accuracy', cv=kf).mean() # 3折交叉验证，有3个准确度分数；注意：这里求了mean()
-#         if score > best_score:
-#             best_score, best_p, best_k = score, p, k
-#
-# print("Best K =", best_k) # 2
-# print("Best P =", best_p) # 3
-# print("Best Score =", best_score) # 0.9833452164435345
-#
-# # 测试：
-# knn_clf = KNeighborsClassifier(weights="distance", n_neighbors=2, p=3)
-# knn_clf.fit(x_train, y_train)
-# testScore = knn_clf.score(x_test, y_test)
-# print("cross_val_score测试结果：", testScore)
+# 2、使用交叉验证：
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+'''
+KFold 测试：
+1、设置shuffle=True时，运行两次，发现多次运行的结果不同。
+2、设置shuffle=True和random_state=整数，发现每次运行的结果都相同
+data = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6])
+# prepare cross validation
+kfold = KFold(n_splits=3, shuffle = True, random_state=42)
+for train, test in kfold.split(data):
+    print('train: %s, test: %s' % (data[train], data[test]))
+'''
+knn_clf = KNeighborsClassifier()
+best_score, best_p, best_k = 0, 0 ,0
+for k in range(2, 11):
+    for p in range(1, 6):
+        knn_clf = KNeighborsClassifier(weights="distance", n_neighbors=k, p=p)  # 定义模型时，手动设定的是 超参数
+        '''
+        一、cross_val_score函数参数：（cross_val_score 只能和 KFold 配合使用，不能和 StratifiedKFold 配合使用）
+        1、scoring参数：
+        1.1、一般来说平均方差(Mean squared error)会用于判断回归(Regression)模型的好坏
+        scoring='mean_squared_error'
+        1.2、平均准确率
+        scoring='accuracy
+        2、cv参数：
+        cv=3：分成3份做交叉验证，2份做训练，1份做验证
+        交叉验证接口sklearn.model_selection.cross_validate没有数据shuffle功能，所以一般结合Kfold一起使用，
+        KFold是做分割数据作用，相当于cross_val_score的cv参数；做交叉验证是cross_val_score。
+
+        二、KFold函数参数：
+        1、设置shuffle=True时，多次运行的结果不同。
+        2、设置shuffle=True和random_state=整数，发现每次运行的结果都相同
+
+        三、理解：
+        单纯从 cross_val_score 来看，每一折交叉验证 模型实例.fit()训练出一个模型（其实就是得到 模型的参数。注意是参数，不是超参数）
+        超参数 是在定义模型时就已经设置好的，不由 cross_val_score 交叉验证 控制。
+        '''
+        kf = KFold(3, shuffle=True)
+        # accuracy 准确率指标。当样本比例不平衡时，该指标不准确。
+        score = cross_val_score(knn_clf, x_train, y_train, scoring='accuracy', cv=kf).mean() # 3折交叉验证，有3个准确度分数；注意：这里求了mean()
+        if score > best_score:
+            best_score, best_p, best_k = score, p, k
+
+print("Best K =", best_k) # 2
+print("Best P =", best_p) # 3
+print("Best Score =", best_score) # 0.9833452164435345
+
+# 测试：
+knn_clf = KNeighborsClassifier(weights="distance", n_neighbors=2, p=3)
+knn_clf.fit(x_train, y_train)
+testScore = knn_clf.score(x_test, y_test)
+print("cross_val_score测试结果：", testScore)
 
 
 
 # 3、使用交叉验证（使用 网格搜索）：同时得到：1、最优模型参数、2、最优模型的超参数
-# '''
+'''
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import StratifiedKFold
 # estimator：创建的对象，如上的knn_clf
@@ -131,4 +131,4 @@ print("GridSearchCV测试结果：", testScore)
 best_knn_clf.fit(x_train[600:1078], y_train[600:1078])
 print(best_knn_clf) # 可以看出 最优模型的超参数保留，改变的只是最优模型的参数
 print("GridSearchCV测试结果：", best_knn_clf.score(x_test, y_test))
-# '''
+'''
