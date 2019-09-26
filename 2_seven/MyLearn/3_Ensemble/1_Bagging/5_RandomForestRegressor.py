a1c8c52a0585a1c6b66cf239d725617895fafd85
blacksevenzqj <397353721@qq.com>
2019-09-26 23:41:38 +0800

    KS阈值

---
+++ 2_seven/MyLearn/3_Ensemble/1_Bagging/5_RandomForestRegressor.py
@@ -0,0 +1,15 @@
+from sklearn.datasets import load_boston  # 一个标签是连续西变量的数据集
+from sklearn.model_selection import cross_val_score  # 导入交叉验证模块
+from sklearn.ensemble import RandomForestRegressor  # 导入随机森林回归系
+
+boston = load_boston()
+regressor = RandomForestRegressor(n_estimators=100, random_state=0, oob_score=True)  # 实例化
+regressor.fit(boston.data, boston.target)
+regressor.score(boston.data, boston.target)  # R方
+
+
+# 如果不写 neg_mean_squared_error，回归评估默认是R平方
+regressor = RandomForestRegressor(n_estimators=100, random_state=0, oob_score=True)  # 实例化
+scores = cross_val_score(regressor, boston.data, boston.target, cv=10
+                         , scoring="neg_mean_squared_error"  # 最小均方差
+                         )
