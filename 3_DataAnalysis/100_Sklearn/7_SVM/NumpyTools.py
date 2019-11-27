f201d5f77582172b6767c97ff2f3a43f622f65a2
blacksevenzqj <397353721@qq.com>
2019-11-27 14:55:52 +0800

    Feature

--- 3_DataAnalysis/100_Sklearn/7_SVM/NumpyTools.py
+++ 3_DataAnalysis/100_Sklearn/7_SVM/NumpyTools.py
@@ -14,19 +14,22 @@ import numpy as np
 [0 0 0 0 1 1 1 2 2 3]
 [0 1 2 3 1 2 3 2 3 3]
 '''
+
+
 def upper_triangular_matrixv(data):
     a = np.triu_indices_from(np.zeros_like(data))[0]
     b = np.triu_indices_from(np.zeros_like(data))[1]
-    c = np.vstack((a, b)) # 堆叠上三角矩阵索引
-    
+    c = np.vstack((a, b))  # 堆叠上三角矩阵索引
+
     result = []
     for i in range(c.shape[1]):
-        result.append(data[c[0,i], c[1,i]])
-    
+        result.append(data[c[0, i], c[1, i]])
+
     return c, result
 
+
 # In[]:
-# numpy数组的拼接、合并  
+# numpy数组的拼接、合并
 # https://blog.csdn.net/qq_39516859/article/details/80666070
 # https://blog.csdn.net/qq_38150441/article/details/80488800
 
@@ -34,89 +37,100 @@ def upper_triangular_matrixv(data):
 # 测试 column_stack 与 hstack 的区别： 1维有区别，2维之后相同。
 def column_stack_and_hstack():
     # 一维
-    a=[1, 2, 3]
-    b=[11, 22, 33]
+    a = [1, 2, 3]
+    b = [11, 22, 33]
     print(a)
     print(b)
-    
-    c = np.column_stack((a,b))
+
+    c = np.column_stack((a, b))
     print(c)
-    
-    d = np.hstack((a,b))
+
+    d = np.hstack((a, b))
     print(d)
-    
-    print(30*"-")
-    
+
+    print(30 * "-")
+
     # 一维
     a = np.array([1, 2, 3])
     b = np.array([11, 22, 33])
     print(a)
     print(b)
-    
-    c = np.column_stack((a,b))
+
+    c = np.column_stack((a, b))
     print(c)
-    
-    d = np.hstack((a,b))
+
+    d = np.hstack((a, b))
     print(d)
-    
-    print(30*"=")
-    
+
+    print(30 * "=")
+
     # 二维：
-    a=[[1],[2],[3]]
-    b=[[11],[12],[13]]
+    a = [[1], [2], [3]]
+    b = [[11], [12], [13]]
     print(a)
     print(b)
-    
-    c = np.column_stack((a,b))
+
+    c = np.column_stack((a, b))
     print(c)
-    
-    d = np.hstack((a,b))
+
+    d = np.hstack((a, b))
     print(d)
-    
-    print(30*"-")
-    
+
+    print(30 * "-")
+
     # 二维：
-    a = np.array([[1],[2],[3]])
-    b = np.array([[11],[12],[13]])
+    a = np.array([[1], [2], [3]])
+    b = np.array([[11], [12], [13]])
     print(a)
     print(b)
-    
-    c = np.column_stack((a,b))
+
+    c = np.column_stack((a, b))
     print(c)
-    
-    d = np.hstack((a,b))
+
+    d = np.hstack((a, b))
     print(d)
-    
-    print(30*"=")
-    
-    a = np.array([[1,2,3],[4,5,6],[7,8,9]])
-    b = np.array([[10,11,12],[13,14,15],[16,17,18]])
+
+    print(30 * "=")
+
+    a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    b = np.array([[10, 11, 12], [13, 14, 15], [16, 17, 18]])
     print(a)
     print(b)
-    
-    c = np.column_stack((a,b))
+
+    c = np.column_stack((a, b))
     print(c)
-    
-    d = np.hstack((a,b))
+
+    d = np.hstack((a, b))
     print(d)
-    
-    print(30*"=")
-    
-    a=[1, 2, 3, 4, 5, 6]
+
+    print(30 * "=")
+
+    a = [1, 2, 3, 4, 5, 6]
     print(a)
     c = np.column_stack(a)
     print(c)
     d = np.hstack(a)
     print(d)
-    
-    print(30*"-")
 
-    a = [[1,2,3],[4,5,6],[7,8,9]]
+    print(30 * "-")
+
+    a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     print(a)
     c = np.column_stack(a)
     print(c)
     d = np.hstack(a)
     print(d)
-    
-    
 
+# # 2.1、数组降维
+# data = np.random.random((2,1))
+# print(data)
+# print(data.ravel())
+#
+# # 2.2、数组增维
+# data = np.arange(0.0, 5.0, 0.1)
+# # print(data)
+# # print(data[:, np.newaxis])
+#
+# l = np.array([1,2,3,4])
+# print(l[:,np.newaxis].shape)
+# print(l[np.newaxis,:].shape)
